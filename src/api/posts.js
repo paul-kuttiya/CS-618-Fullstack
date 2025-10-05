e5bf79a670a4e43d09bd4911180f5aa57089e224
Paul Kuttiya <w.kuttiya@gmail.com>
2025-10-05 01:30:34 -0400

    Module 8 (#12)
    * add ssr
    
    * add seo
    
    * add seo done
    
    * add helmet
    
    * meta tags
    
    * dockerfile
    
    * fix lint

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,23 +1,35 @@
-export const getPosts = async (queryParams) => {
-		const res = await fetch(
-				`${import.meta.env.VITE_BACKEND_URL}/posts?` +
-						new URLSearchParams(queryParams),
-		)
-		return await res.json()
+/* eslint-disable no-undef */
+const API_BASE =
+  (typeof window === 'undefined'
+    ? (process.env.SSR_BACKEND_URL || process.env.VITE_BACKEND_URL)
+    : import.meta.env.VITE_BACKEND_URL) ?? import.meta.env.VITE_BACKEND_URL
+
+export const getPosts = async (queryParams = {}) => {
+  const qs = new URLSearchParams(queryParams).toString()
+  const url = `${API_BASE}/posts${qs ? `?${qs}` : ''}`
+  const res = await fetch(url, { headers: { accept: 'application/json' } })
+  if (!res.ok) throw new Error(`GET ${url} -> ${res.status}`)
+  return res.json()
 }
 
 export const createPost = async (token, post) => {
-		const res = await fetch(
-				`${import.meta.env.VITE_BACKEND_URL}/
-						posts`,
-				{
-						method: 'POST',
-						headers: {
-								'Content-Type': 'application/json',
-								Authorization: `Bearer ${token}`,
-						},
-						body: JSON.stringify(post),
-				},
-		)
-		return await res.json()
+  const url = `${API_BASE}/posts`
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      accept: 'application/json',
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+    },
+    body: JSON.stringify(post),
+  })
+  if (!res.ok) throw new Error(`POST ${url} -> ${res.status}`)
+  return res.json()
+}
+
+export const getPostById = async (postId) => {
+  const url = `${API_BASE}/posts/${encodeURIComponent(postId)}`
+  const res = await fetch(url, { headers: { accept: 'application/json' } })
+  if (!res.ok) throw new Error(`GET ${url} -> ${res.status}`)
+  return res.json()
 }
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,31 +3,42 @@ import { CreatePost } from '../components/CreatePost.jsx'
 import { PostFilter } from '../components/PostFilter.jsx'
 import { PostSorting } from '../components/PostSorting.jsx'
 import { Header } from '../components/Header.jsx'
+import { Helmet } from '@dr.pogodin/react-helmet'
 import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { getPosts } from '../api/posts.js'
+
 export function Blog() {
-		const [author, setAuthor] = useState('')
-		const [sortBy, setSortBy] = useState('createdAt')
-		const [sortOrder, setSortOrder] = useState('descending')
-		const postsQuery = useQuery({
-				queryKey: ['posts', { author, sortBy, sortOrder }],
-				queryFn: () => getPosts({ author, sortBy, sortOrder }),
-		})
-		const posts = postsQuery.data ?? []
-		return (
-				<div style={{ padding: 8 }}>
-						<Header />
-						<br />
-						<hr />
-						<br />
-						<h1>Welcome to My Blog!</h1>
-						<CreatePost />
+  const [author, setAuthor] = useState('')
+  const [sortBy, setSortBy] = useState('createdAt')
+  const [sortOrder, setSortOrder] = useState('descending')
+
+  const postsQuery = useQuery({
+    queryKey: ['posts', { author, sortBy, sortOrder }],
+    queryFn: () => getPosts({ author, sortBy, sortOrder }),
+  })
+
+  const posts = postsQuery.data ?? []
+
+  return (
+    <div style={{ padding: 8 }}>
+      <Helmet>
+        <title>Full-Stack React Blog</title>
+        <meta
+          name="description"
+          content="A blog full of articles about full-stack React development."
+        />
+      </Helmet>
+
+      <Header />
+      <br />
+      <hr />
+      <CreatePost />
       <br />
       <hr />
       Filter by:
       <PostFilter
-        field='author'
+        field="author"
         value={author}
         onChange={(value) => setAuthor(value)}
       />
