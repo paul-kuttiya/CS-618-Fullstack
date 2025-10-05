9879d29cd8f825f6331a21b43db3fe1667e4cb3c
Paul Kuttiya <w.kuttiya@gmail.com>
2025-10-05 17:08:38 +0000

    module 9

--- src/pages/ViewPost.jsx
+++ src/pages/ViewPost.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { useQuery, useMutation } from '@tanstack/react-query'
 import { Header } from '../components/Header.jsx'
 import { Post } from '../components/Post.jsx'
 import { getPostById } from '../api/posts.js'
+import { PostStats } from '../components/PostStats.jsx'
 import { useEffect, useState } from 'react'
 import { postTrackEvent } from '../api/events.js'
 import { getUserInfo } from '../api/users.js'
@@ -43,7 +45,11 @@ export function ViewPost({ postId }) {
 
   function truncate(str, max = 160) {
     if (!str) return str
-    return str.length > max ? str.slice(0, max - 3) + '...' : str
+    if (str.length > max) {
+      return str.slice(0, max - 3) + '...'
+    } else {
+      return str
+    }
   }
 
   return (
@@ -54,15 +60,11 @@ export function ViewPost({ postId }) {
           <meta name="description" content={truncate(post.contents)} />
           <meta property="og:type" content="article" />
           <meta property="og:title" content={post.title} />
-          <meta property="article:published_time" content={post.createdAt} />
-          {post.updatedAt && (
-            <meta property="article:modified_time" content={post.updatedAt} />
-          )}
-          {userInfo.username && (
-            <meta property="article:author" content={userInfo.username} />
-          )}
+          <meta property="og:article:published_time" content={post.createdAt} />
+          <meta property="og:article:modified_time" content={post.updatedAt} />
+          <meta property="og:article:author" content={userInfo.username} />
           {(post.tags ?? []).map((tag) => (
-            <meta key={tag} property="article:tag" content={tag} />
+            <meta key={tag} property="og:article:tag" content={tag} />
           ))}
         </Helmet>
       )}
@@ -73,7 +75,15 @@ export function ViewPost({ postId }) {
       <Link to="/">Back to main page</Link>
       <br />
       <hr />
-      {post ? <Post {...post} fullPost /> : `Post with id${postId} not found.`}
+      {post ? (
+        <div>
+          <Post {...post} fullPost />
+          <hr />
+          <PostStats postId={postId} />
+        </div>
+      ) : (
+        `Post with id ${postId} not found.`
+      )}
     </div>
   )
 }
