74dedbe8bb4c7821bda576d10b16a49afa5b14d3
Paul Kuttiya <w.kuttiya@gmail.com>
2025-10-05 16:42:33 +0000

    frontend module-9.4

--- src/pages/ViewPost.jsx
+++ src/pages/ViewPost.jsx
@@ -1,13 +1,33 @@
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useMutation } from '@tanstack/react-query'
 import { Header } from '../components/Header.jsx'
 import { Post } from '../components/Post.jsx'
 import { getPostById } from '../api/posts.js'
+import { useEffect, useState } from 'react'
+import { postTrackEvent } from '../api/events.js'
 import { getUserInfo } from '../api/users.js'
 import { Helmet } from '@dr.pogodin/react-helmet'
 
 export function ViewPost({ postId }) {
+  const [session, setSession] = useState()
+
+  const trackEventMutation = useMutation({
+    mutationFn: (action) => postTrackEvent({ postId, action, session }),
+    onSuccess: (data) => setSession(data?.session),
+  })
+
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      trackEventMutation.mutate('startView')
+      timeout = null
+    }, 1000)
+    return () => {
+      if (timeout) clearTimeout(timeout)
+      else trackEventMutation.mutate('endView')
+    }
+  }, [])
+
   const postQuery = useQuery({
     queryKey: ['post', postId],
     queryFn: () => getPostById(postId),
@@ -32,10 +52,8 @@ export function ViewPost({ postId }) {
         <Helmet>
           <title>{post.title} | Full-Stack React Blog</title>
           <meta name="description" content={truncate(post.contents)} />
-          {/* Open Graph */}
           <meta property="og:type" content="article" />
           <meta property="og:title" content={post.title} />
-          {/* Article-specific OG properties (no 'og:' prefix for these keys) */}
           <meta property="article:published_time" content={post.createdAt} />
           {post.updatedAt && (
             <meta property="article:modified_time" content={post.updatedAt} />
